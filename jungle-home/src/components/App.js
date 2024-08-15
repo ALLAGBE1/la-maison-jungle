4beea7c959d482402030048fd507f75badeafb7c
degnonepithech <josue.allagbe@epitech.eu>
2024-08-15 17:16:42 +0100

    Create component Banner

---
+++ jungle-home/src/components/App.js
@@ -0,0 +1,11 @@
+import '../App.css';
+import React from 'react';
+import Banner from "./Banner";
+import Cart from "./Cart";
+
+
+function App() {
+  return <React.Fragment><Banner /><Cart /></React.Fragment>;
+}
+
+export default App;
