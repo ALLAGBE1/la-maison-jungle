0aaa91597bc66a751d0c91094fccf4679c6393cd
degnonepithech <josue.allagbe@epitech.eu>
2024-08-17 02:06:32 +0100

    Exo-P2C3-1

--- jungle-home/src/components/App.js
+++ jungle-home/src/components/App.js
@@ -2,10 +2,15 @@ import '../App.css';
 import React from 'react';
 import Banner from "./Banner";
 import Cart from "./Cart";
+import ShoppingList from "./ShoppingList"
 
 
 function App() {
-  return <React.Fragment><Banner /><Cart /></React.Fragment>;
+  return <React.Fragment>
+          <Banner />
+          <Cart />
+          <ShoppingList />
+        </React.Fragment>;
 }
 
 export default App;
