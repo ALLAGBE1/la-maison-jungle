014b44e0a8a8a3a92d2e4316b65ba5daa1f2cbd3
degnonepithech <josue.allagbe@epitech.eu>
2024-08-18 02:45:14 +0100

    Exo-P2C3-2

--- jungle-home/src/components/ShoppingList.js
+++ jungle-home/src/components/ShoppingList.js
@@ -1,19 +1,34 @@
 import { plantList } from '../datas/plantList'
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
-    const categories = plantList.reduce((acc, plant) => acc.includes(plant.category) ? acc : acc.concat(plant.category),  []);
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
 
-	return (<div>
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map((plant) => (
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.name}
+                        {plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
+					</li>
+				))}
+			</ul>
 
-        <ul> {categories.map(cat => 
-            <li key={cat}> {cat} </li>
-        )} </ul>
-    
-        <ul>{ plantList.map(plant => {
-            return <li key={plant.id}>{plant.name}</li>
-        }) }</ul>
-    </div>)
+            <div className='lmj-sales'>
+                
+            </div>
+		</div>
+	)
 }
 
 export default ShoppingList
-
